1cfb09580d1fa7b32af1aedad6a458d2bf5550e5
Alexandra Danilevich <bloodangelalexandra@gmail.com>
2024-02-03 15:33:09 +0300

    Fix header

--- src/components/LoginPopup/loginPopup.component.tsx
+++ src/components/LoginPopup/loginPopup.component.tsx
@@ -1,81 +1,78 @@
-import { Avatar, Button, Checkbox, CssBaseline, FormControlLabel, Grid, Link, TextField, Typography } from "@mui/material";
-import Modal from '@mui/material/Modal';
+import {
+	Avatar,
+	Button,
+	Checkbox,
+	CssBaseline,
+	FormControlLabel,
+	Grid,
+	Link,
+	TextField,
+	Typography,
+} from "@mui/material";
+import Modal from "@mui/material/Modal";
 import { Box, Container } from "@mui/system";
 
-function LoginPopup({ open, handleClose }: { open: boolean, handleClose: any }) {
+function LoginPopup({ open, handleClose }: { open: boolean; handleClose: any }) {
+	const handleSubmit = () => {
+		console.log("kk");
+	};
 
-    const handleSubmit = () => {
-        console.log('kk');
-
-    }
-
-    return (
-        <Modal
-            open={open}
-            onClose={handleClose}
-        >
-            <Box>
-                <CssBaseline />
-                <Box
-                    sx={{
-                        marginTop: 8,
-                        display: 'flex',
-                        flexDirection: 'column',
-                        alignItems: 'center',
-                    }}
-                >
-                    <Typography component="h1" variant="h5">
-                        Sign in
-                    </Typography>
-                    <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
-                        <TextField
-                            margin="normal"
-                            required
-                            fullWidth
-                            id="email"
-                            label="Email Address"
-                            name="email"
-                            autoComplete="email"
-                            autoFocus
-                        />
-                        <TextField
-                            margin="normal"
-                            required
-                            fullWidth
-                            name="password"
-                            label="Password"
-                            type="password"
-                            id="password"
-                            autoComplete="current-password"
-                        />
-                        <FormControlLabel
-                            control={<Checkbox value="remember" color="primary" />}
-                            label="Remember me"
-                        />
-                        <Button
-                            fullWidth
-                            variant="contained"
-                            sx={{ mt: 3, mb: 2 }}
-                        >
-                            Sign In
-                        </Button>
-                        <Grid container>
-                            <Grid item xs>
-                                <Link href="#" variant="body2">
-                                    Forgot password?
-                                </Link>
-                            </Grid>
-                            <Grid item>
-                                <Link href="#" variant="body2">
-                                    {"Don't have an account? Sign Up"}
-                                </Link>
-                            </Grid>
-                        </Grid>
-                    </Box>
-                </Box>
-            </Box>
-        </Modal>
-    );
+	return (
+		<Modal open={open} onClose={handleClose}>
+			<Box>
+				<CssBaseline />
+				<Box
+					sx={{
+						marginTop: 8,
+						display: "flex",
+						flexDirection: "column",
+						alignItems: "center",
+					}}>
+					<Typography component="h1" variant="h5">
+						Sign in
+					</Typography>
+					<Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
+						<TextField
+							margin="normal"
+							required
+							fullWidth
+							id="email"
+							label="Email Address"
+							name="email"
+							autoComplete="email"
+							autoFocus
+						/>
+						<TextField
+							margin="normal"
+							required
+							fullWidth
+							name="password"
+							label="Password"
+							type="password"
+							id="password"
+							autoComplete="current-password"
+						/>
+						<FormControlLabel control={<Checkbox value="remember" color="primary" />} label="Remember me" />
+						<Button fullWidth variant="contained" sx={{ mt: 3, mb: 2 }}>
+							Sign In
+						</Button>
+						<Grid container>
+							<Grid item xs>
+								<Link href="#" variant="body2">
+									Forgot password?
+								</Link>
+							</Grid>
+							<Grid item>
+								<Link href="#" variant="body2">
+									{"Don't have an account? Sign Up"}
+								</Link>
+							</Grid>
+						</Grid>
+					</Box>
+				</Box>
+			</Box>
+		</Modal>
+	);
 }
 
 export default LoginPopup;
--- src/pages/CurrentMovie/styles.ts
+++ src/pages/CurrentMovie/styles.ts
@@ -2,18 +2,25 @@ import { CSSProperties } from "react";
 import { CustomSxProps, UI_KIT } from "../../theme";
 
 export const wrapper: CustomSxProps = {
-  color: UI_KIT.textColor,
-  width: "100%",
+	color: UI_KIT.textColor,
+	width: "100%",
 };
 
 export const infoWrapper: CustomSxProps = {
-  height: "50%",
-  width: "100%",
-  display: "flex",
-  columnGap: UI_KIT.p16,
+	height: "60vh",
+	width: "100%",
+	display: "flex",
+	columnGap: UI_KIT.p16,
 };
 
 export const imgStyles: CSSProperties = {
-  height: "100%",
-  width: "100%",
+	height: "100%",
+	width: "100%",
+	borderRadius: UI_KIT.p16,
+};
+
+export const infoText: CustomSxProps = {
+	backgroundColor: (theme) => theme.palette.grey[200],
+	width: "100%",
+	borderRadius: UI_KIT.p16,
 };
