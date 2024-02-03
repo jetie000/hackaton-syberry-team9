e022c1fda84b785940af869dcbbd52d4ef847846
Alexandra Danilevich <bloodangelalexandra@gmail.com>
2024-02-03 12:03:53 +0300

    add global css

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,9 +1,14 @@
 import React from "react";
 
+import {ReactComponent as Logo} from "../../assets/logo.svg"
+
 import styles from "./styles.module.scss";
+import { Box } from "@mui/material";
 
 const Header = () => {
-  return <header className={styles.wrapper}></header>;
+  return <Box className={styles.wrapper} component="header">
+    <Logo/>
+  </Box>;
 };
 
 export default Header;
