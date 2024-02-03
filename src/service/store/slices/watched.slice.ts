538dd16f7399464c3798833d896cb2ef62d78d61
Alexandra Danilevich <bloodangelalexandra@gmail.com>
2024-02-03 17:45:24 +0300

    Add slider to currentmovie

--- src/service/store/slices/watched.slice.ts
+++ src/service/store/slices/watched.slice.ts
@@ -1,30 +1,28 @@
 import { variables } from "@/helpers/variables";
 import { PayloadAction, createSlice } from "@reduxjs/toolkit";
 
-
 export interface watchedState {
-    watched?: number[]
+	watched?: number[];
 }
 
 const initialState: watchedState = {
-    watched: JSON.parse(localStorage.getItem(variables.WATCHED_LOCALSTORAGE)!) || undefined
-}
+	watched: JSON.parse(localStorage.getItem(variables.WATCHED_LOCALSTORAGE)!) || undefined,
+};
 
 export const watchedSlice = createSlice({
-    name: 'watched',
-    initialState,
-    reducers: {
-        addWatched: (state, { payload: id }: PayloadAction<number>) => {
-            const findId = state.watched?.find(i => i === id)
-            if(!findId){
-                state.watched?.push(id)
-            }
-            else if(findId !== (state.watched?.length || 0) - 1 ){
-                state.watched?.splice(findId, 1)
-                state.watched?.push(id)
-            }
-        }
-    },
-})
+	name: "watched",
+	initialState,
+	reducers: {
+		addWatched: (state, { payload: id }: PayloadAction<number>) => {
+			const findId = state.watched?.find((i) => i === id);
+			if (!findId) {
+				state.watched?.push(id);
+			} else if (findId !== (state.watched?.length || 0) - 1) {
+				state.watched?.splice(findId, 1);
+				state.watched?.push(id);
+			}
+		},
+	},
+});
 
-export const { actions, reducer } = watchedSlice
+export const { actions, reducer } = watchedSlice;
