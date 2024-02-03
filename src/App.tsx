ec374ab87338b23366055d38bd0fa7de550e59c6
jetie000 <makeenko.005@gmail.com>
2024-02-03 14:04:41 +0300

    feat: add login modal

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { Provider } from "react-redux";
 import { store } from "./service/store/store";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Header from "./components/Header";
+import LoginPopup from "./components/LoginPopup/loginPopup.component";
 
 function App() {
   return (
