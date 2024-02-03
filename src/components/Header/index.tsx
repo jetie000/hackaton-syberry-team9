4137ecefd6567548afebcefb0281ae0a18a94d73
Alexandra Danilevich <bloodangelalexandra@gmail.com>
2024-02-03 11:13:55 +0300

    pre fetch

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,14 @@
+import React from "react"
+
+import styles from "./styles.module.scss"
+
+const Header = () => {
+    return (
+        <header className={styles.wrapper}>
+
+        </header>
+    )
+
+}
+
+export default Header;
