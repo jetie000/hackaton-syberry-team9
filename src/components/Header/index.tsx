2a36ce115d5a05733a9b4622cffb7eff97c87216
Alexandra Danilevich <bloodangelalexandra@gmail.com>
2024-02-03 13:11:01 +0300

    add new sxprops

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -3,11 +3,25 @@ import React from "react";
 import {ReactComponent as Logo} from "../../assets/logo.svg"
 
 import styles from "./styles.module.scss";
-import { Box } from "@mui/material";
+import { Box, Button, TextField, Typography } from "@mui/material";
+import { grey } from "@mui/material/colors";
+import { wrapper } from "./styles";
 
 const Header = () => {
-  return <Box className={styles.wrapper} component="header">
-    <Logo/>
+    const loggedIn = false;
+  return <Box sx={wrapper} component="header">
+    <Box>
+        <Logo className={styles.icon}/>
+        <Typography m="auto 0">КиноСкрыт</Typography>
+    </Box>
+    <Box className={styles.searchBar}>
+        <Box></Box>
+        <Box>
+            <TextField size="small" placeholder="search"/>
+            <Button variant="contained">Find Me</Button>
+        </Box>
+        {loggedIn?<Box></Box>:<Button className={styles.secondaryButton} variant="outlined" size="small">Log In</Button>}
+    </Box>
   </Box>;
 };
 
